e94d192cf6374f1c0791cc0bc0c06c7e21cdf62e
vagrant <vagrant@homestead>
2020-01-05 10:31:31 +0000

    用户可以发表评论

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -26,7 +26,7 @@
     </div>
 
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           <h1 class="text-center mt-3 mb-3">
             {{ $topic->title }}
@@ -42,6 +42,7 @@
           <div class="topic-body mt-4 mb-4">
             {!! $topic->body !!}
           </div>
+
           @can('update', $topic)
             <div class="operate">
               <hr>
@@ -62,6 +63,15 @@
 
         </div>
       </div>
+
+      {{-- 用户回复列表 --}}
+      <div class="card topic-reply mt-4">
+          <div class="card-body">
+              @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+              @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+          </div>
+      </div>
+
     </div>
   </div>
 @stop
